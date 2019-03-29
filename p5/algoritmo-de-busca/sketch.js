d5a7f46437a1386d6f52e4747b746960ef3df1f6
matheus-rn <matheus.1nascimento58@gmail.com>
2019-03-29 10:17:12 -0300

    Busca binária

--- p5/algoritmo-de-busca/sketch.js
+++ p5/algoritmo-de-busca/sketch.js
@@ -2,7 +2,7 @@
 var x = 0;
 var elementos = [];
 var valorAleaotrio = 0;
-var tamanhoVetor
+var tamanhoVetor;
 
 class circulo {
   
@@ -11,9 +11,14 @@ class circulo {
     this.posY = posY;
     this.diametro = diametro;
     this.pintado = false;
+    this.speed = 0;
   }  
   show(){
     circle(this.posX,this.posY,this.diametro);
+  }
+
+  move(){
+    this.posY -= 1; 
   }
 
   isPintado(){
@@ -25,51 +30,54 @@ function preencheVetor(){
   tamanhoVetor = parseInt(random(3,18));
   valorAleaotrio = parseInt(random(0,tamanhoVetor+1))
   for(let i = 0;i < tamanhoVetor+1;i++){
-    elementos.push(new circulo(50 * i + 100,300,15))
+    elementos.push(new circulo(50 * i + (600-((tamanhoVetor/2)*50)),300,15))
   }
   elementos[valorAleaotrio].pintado = true;
  
 }
 
 function setup() {
-  createCanvas(1000,600)
+  createCanvas(1200,600)
   frameRate(1.5);
   preencheVetor();
   textSize(40);
-  textAlign(CENTER);
-
-  
+  textAlign(CENTER);  
 }
 
 function draw() {
   background(0);
   escreve(false);
-  console.log("Valor aleatorio: "+valorAleaotrio);
-  console.log("Tamanho do vetor: "+tamanhoVetor);
   for(let i = 0; i < tamanhoVetor+1;i++){
     if(elementos[i].pintado == true){
       fill(255,0,0);
     }else{
       fill(255);
     }
-    elementos[i].show()
+    elementos[i].show();
   }
-  fill(0,255,0)
-  elementos[x].show()
+
+  fill(0,255,0);
+  elementos[x].show();
   
   if(x === valorAleaotrio){
-    escreve(true)
-    noLoop();
+    elementos[x].move();
+    frameRate(70);
+    if(elementos[x].posY == 250){
+      escreve(true);
+      noLoop();
+    }
+  }else{
+    x++;
   }
-  x++;
-}
 
+}
+ 
 function escreve(terminou){
  
   fill(0,255,0)
-  text('Busca sequencial',400,60);
+  text('Busca sequencial',600,60);
 
   if(terminou === true){
-    text('Encontrou o valor',400,500)
+    text('Encontrou o valor!',600,500)
   }
 }
